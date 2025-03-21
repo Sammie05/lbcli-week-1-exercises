2919ed5e2ca3815d5ae48ccc93edd2227fd2fff2
Sammie05 <snweke98@gmail.com>
2025-03-21 02:37:59 +0100

    updated

--- submission/test.sh
+++ submission/test.sh
@@ -178,7 +178,7 @@ check_cmd "Getting address info"
 # WRITE YOUR SOLUTION BELOW:
 SCRIPT_PUBKEY=$(echo "$ADDR_INFO" | jq -r '.scriptPubKey')
 check_cmd "Extracting key from descriptor"
-INTERNAL_KEY="$(SCRIPT_PUBKEY:4")
+INTERNAL_KEY="$(SCRIPT_PUBKEY:4)"
 
 # STUDENT TASK: Create a proper descriptor with just the key
 # WRITE YOUR SOLUTION BELOW:
@@ -195,7 +195,7 @@ echo "Taproot treasure map: $TAPROOT_DESCRIPTOR"
 
 # STUDENT TASK: Derive an address from the descriptor
 # WRITE YOUR SOLUTION BELOW:
-DERIVED_ADDR_RAW=$(bitcoin-cli -regtest deriveaddresses "$TAPROOT_DESCRIPTOR" | jq -r '.[0]')
+DERIVED_ADDR_RAW=$(bitcoin-cli -regtest deriveaddresses "${TAPROOT_DESCRIPTOR}" | jq -r '.[0]')
 check_cmd "Address derivation"
 DERIVED_ADDR=$(echo "$DERIVED_ADDR_RAW" | tr -d '[:space:]')
 echo "Derived quantum vault address: $DERIVED_ADDR"
